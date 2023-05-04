cf929ccc90d94b0c6e3dded57dd90479fe60753e
IqBatz <davidbatzovalle@gmail.com>
2023-05-04 17:15:06 -0600

    asd

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const myconn = require('express-myconnection')
-const routes = require('./routes')
+const routes = require('./inventario/routes')
 const cors = require('cors')
 
 
